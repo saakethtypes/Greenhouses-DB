61f5e25e9f5ed2617cc663c9a5c50f8be2f366ff
Satwika-M <maddukuri.s@northeastern.edu>
2024-11-07 20:56:48 -0500

    add insert_admin_data.sql

---
+++ insert_admin_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO GREENHOUSES (greenhouse_id, location, total_slots) VALUES
+(1, 'North Wing', 100);
+INSERT INTO GREENHOUSES (greenhouse_id, location, total_slots) VALUES
+(2, 'South Wing', 150);
+INSERT INTO GREENHOUSES (greenhouse_id, location, total_slots) VALUES
+(3, 'East Wing', 125);
+INSERT INTO GREENHOUSES (greenhouse_id, location, total_slots) VALUES
+(4, 'West Wing', 175);
+INSERT INTO GREENHOUSES (greenhouse_id, location, total_slots) VALUES
+(5, 'Central Wing', 200);
