5fbf2073468dd825eb64deeb5d13eb1fd4c95a04
Satwika-M <maddukuri.s@northeastern.edu>
2024-11-07 23:26:44 -0500

    Update role_grants.sql

--- role_grants.sql
+++ role_grants.sql
@@ -1,18 +1,20 @@
 -- Technician privileges
-GRANT SELECT ON PLANT_BEDS TO technician_role;
-GRANT SELECT ON CROP_TYPES TO technician_role;
-GRANT SELECT ON SENSORS TO technician_role;
-GRANT SELECT, INSERT, UPDATE ON SENSOR_LOGS TO technician_role;
-GRANT SELECT ON GROWTH_CYCLE TO technician_role;
+GRANT SELECT ON hydro_admin.PLANT_BEDS TO technician_role;
+GRANT SELECT ON hydro_admin.CROP_TYPES TO technician_role;
+GRANT SELECT, INSERT, UPDATE ON hydro_admin.SENSORS TO technician_role;
+GRANT SELECT, INSERT, UPDATE ON hydro_admin.SENSOR_LOGS TO technician_role;
+GRANT SELECT ON hydro_admin.GROWTH_CYCLE TO technician_role;
 
 -- Agronomist privileges
-GRANT SELECT, UPDATE ON PLANT_BEDS TO agronomist_role;
-GRANT SELECT, UPDATE ON CROP_TYPES TO agronomist_role;
-GRANT SELECT, UPDATE ON GROWTH_CYCLE TO agronomist_role;
-GRANT SELECT ON SENSOR_LOGS TO agronomist_role;
+GRANT SELECT, INSERT, UPDATE ON hydro_admin.PLANT_BEDS TO agronomist_role;
+GRANT SELECT, INSERT, UPDATE ON hydro_admin.CROP_TYPES TO agronomist_role;
+GRANT SELECT, INSERT, UPDATE ON hydro_admin.GROWTH_CYCLE TO agronomist_role;
+GRANT SELECT, INSERT, UPDATE ON hydro_admin.SENSOR_LOGS TO agronomist_role;
+GRANT SELECT, INSERT ON hydro_admin.HARVESTED_CROPS TO agronomist_role;
 
 -- Sales Manager privileges
-GRANT SELECT ON HARVESTED_CROPS TO sales_manager_role;
-GRANT SELECT ON ORDERS TO sales_manager_role;
-GRANT SELECT ON ORDER_ITEMS TO sales_manager_role;
-GRANT SELECT ON CUSTOMERS TO sales_manager_role;
+GRANT SELECT, UPDATE ON hydro_admin.HARVESTED_CROPS TO sales_manager_role;
+GRANT SELECT,INSERT, UPDATE ON hydro_admin.ORDERS TO sales_manager_role;
+GRANT SELECT,INSERT, UPDATE ON hydro_admin.ORDER_ITEMS TO sales_manager_role;
+GRANT SELECT,INSERT, UPDATE ON hydro_admin.CUSTOMERS TO sales_manager_role;
+
