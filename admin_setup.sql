37ee1e870e29ce039b3a47581ff8c297934144cc
Saaketh Chaganty <47172497+saakethtypes@users.noreply.github.com>
2024-12-02 16:28:48 -0500

    Update admin_setup.sql

--- admin_setup.sql
+++ admin_setup.sql
@@ -60,5 +60,21 @@ GRANT INSERT ANY TABLE TO hydro_admin;
 GRANT UPDATE ANY TABLE TO hydro_admin;
 GRANT DELETE ANY TABLE TO hydro_admin;
 GRANT CREATE PUBLIC SYNONYM TO hydro_admin;
+GRANT DROP PUBLIC SYNONYM TO hydro_admin;
 
+
+-- Grant schema-level privileges to hydro_admin
+GRANT CREATE PROCEDURE TO hydro_admin;
+GRANT CREATE TRIGGER TO hydro_admin;
+GRANT CREATE TABLE TO hydro_admin;
+GRANT CREATE VIEW TO hydro_admin;
+GRANT CREATE SEQUENCE TO hydro_admin;
+GRANT CREATE ANY INDEX TO hydro_admin;
+GRANT ALTER ANY TABLE TO hydro_admin;
+GRANT ALTER ANY PROCEDURE TO hydro_admin;
+GRANT ALTER ANY TRIGGER TO hydro_admin;
+GRANT DEBUG ANY PROCEDURE TO hydro_admin;
+
+
+-- RESET 
 --drop user hydro_admin cascade;
