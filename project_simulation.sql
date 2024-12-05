0d607aa6783d43ae30fd28ebe1c732895f775a5c
Saaketh Chaganty <47172497+saakethtypes@users.noreply.github.com>
2024-12-04 23:51:51 -0500

    Update project_simulation.sql

--- project_simulation.sql
+++ project_simulation.sql
@@ -33,9 +33,7 @@ CREATE OR REPLACE PACKAGE BODY hydro_admin.simulation_pkg IS
             -- Use the package procedure to log the reading
             sensor_mgmt_pkg.log_sensor_reading(
                 p_sensor_id => sensor_rec.sensor_id,
-                p_reading_value => ROUND(v_reading_value, 2),
-                p_timestamp => CAST(p_current_date AS TIMESTAMP),
-                p_log_id => v_log_id
+                p_reading_value => ROUND(v_reading_value, 2)
             );
         END LOOP;
     END generate_sensor_readings;
@@ -49,7 +47,7 @@ CREATE OR REPLACE PACKAGE BODY hydro_admin.simulation_pkg IS
                 gc.crop_type_id,
                 ct.harvest_time_days,
                 ct.crop_name,
-                ROUND(((p_current_date - gc.start_date) / ct.harvest_time_days) * 100) as progress_pct
+                ROUND(abs((p_current_date - gc.start_date) / ct.harvest_time_days) * 100) as progress_pct
             FROM growth_cycle gc
             JOIN crop_types ct ON gc.crop_type_id = ct.crop_type_id
             WHERE gc.stage != 'Harvest';
@@ -86,7 +84,7 @@ CREATE OR REPLACE PACKAGE BODY hydro_admin.simulation_pkg IS
                                    v_rec.stage || ' to ' || v_new_stage);
                 
                 -- Use crop management package to update growth cycle stage
-                facility_mgmt_pkg.update_growth_cycle_stage(
+                harvest_mgmt_pkg.update_growth_cycle_stage(
                     p_growth_cycle_id => v_rec.growth_cycle_id,
                     p_new_stage => v_new_stage
                 );
@@ -100,140 +98,175 @@ CREATE OR REPLACE PACKAGE BODY hydro_admin.simulation_pkg IS
             END IF;
             RAISE;
     END progress_growth_cycles;
-    
-    PROCEDURE generate_daily_orders(p_simulation_date IN DATE) IS
+
+PROCEDURE generate_daily_orders(p_simulation_date IN DATE) IS
     v_order_id NUMBER;
     v_order_item_id NUMBER;
     v_order_qty NUMBER;
-    v_num_retail_customers NUMBER;
-    v_retail_count NUMBER := 0;
+    v_items_added BOOLEAN;
+    v_successful BOOLEAN;
 BEGIN
-    -- Process wholesale customers
-    FOR wholesale_rec IN (
+    -- Process wholesale customers (10-12 orders each)
+    FOR wholesale IN (
         SELECT customer_id 
         FROM customers 
-        WHERE UPPER(last_name) = 'WHOLESALE'
+        WHERE UPPER(last_name) LIKE '%WHOLESALE%'
     ) LOOP
         -- Generate 10-12 orders per wholesale customer
         FOR i IN 1..TRUNC(DBMS_RANDOM.VALUE(10, 13)) LOOP
-            -- Create order using package procedure
-            order_mgmt_pkg.create_order(
-                p_customer_id => wholesale_rec.customer_id,
-                p_order_date => p_simulation_date,
-                p_new_order_id => v_order_id
-            );
+            v_items_added := FALSE;
             
-            -- Add 3-5 items per wholesale order
-            FOR j IN 1..TRUNC(DBMS_RANDOM.VALUE(3, 6)) LOOP
-                -- Try to add an item from available harvest
-                FOR harvest_rec IN (
-                    SELECT 
-                        hc.harvest_id,
-                        hc.available_quantity_kg
-                    FROM harvested_crops hc
-                    WHERE hc.available_quantity_kg > 0
-                    ORDER BY DBMS_RANDOM.VALUE()
-                ) LOOP
-                    -- Calculate random order quantity (2-10 kg for wholesale)
-                    v_order_qty := ROUND(DBMS_RANDOM.VALUE(2, 10), 1);
+            -- Create new order using package procedure
+            BEGIN
+                ORDER_MGMT_PKG.CREATE_ORDER(
+                    P_CUSTOMER_ID => wholesale.customer_id,
+                    P_ORDER_DATE => p_simulation_date,
+                    P_NEW_ORDER_ID => v_order_id
+                );
+                
+                -- Add 3-5 items per order
+                FOR j IN 1..TRUNC(DBMS_RANDOM.VALUE(3, 6)) LOOP
+                    v_successful := FALSE;
                     
-                    IF harvest_rec.available_quantity_kg >= v_order_qty THEN
-                        -- Add order item using package procedure
-                        order_mgmt_pkg.add_order_item(
-                            p_order_id => v_order_id,
-                            p_harvest_id => harvest_rec.harvest_id,
-                            p_quantity_kg => v_order_qty,
-                            p_order_item_id => v_order_item_id
-                        );
-                        EXIT;  -- Move to next item
-                    END IF;
+                    -- Try different harvests until one succeeds or we run out of options
+                    FOR harvest IN (
+                        SELECT h.harvest_id, h.available_quantity_kg
+                        FROM harvested_crops h
+                        WHERE h.available_quantity_kg > 0
+                        AND h.harvest_id NOT IN (
+                            SELECT harvest_id FROM order_items 
+                            WHERE order_id = v_order_id
+                        )
+                        ORDER BY DBMS_RANDOM.VALUE
+                    ) LOOP
+                        EXIT WHEN v_successful;
+                        
+                        BEGIN
+                            -- Generate order quantity (2-10kg for wholesale)
+                            v_order_qty := ROUND(DBMS_RANDOM.VALUE(2, LEAST(10, harvest.available_quantity_kg)), 1);
+                            
+                            -- Try to add order item
+                            ORDER_MGMT_PKG.ADD_ORDER_ITEM(
+                                P_ORDER_ID => v_order_id,
+                                P_HARVEST_ID => harvest.harvest_id,
+                                P_QUANTITY_KG => v_order_qty,
+                                P_ORDER_ITEM_ID => v_order_item_id
+                            );
+                            
+                            v_successful := TRUE;
+                            v_items_added := TRUE;
+                        EXCEPTION
+                            WHEN OTHERS THEN
+                                -- Just continue to next harvest
+                                NULL;
+                        END;
+                    END LOOP;
                 END LOOP;
-            END LOOP;
+                
+            EXCEPTION
+                WHEN OTHERS THEN
+                    -- Log error and continue with next order
+                    DBMS_OUTPUT.PUT_LINE('Error processing wholesale order: ' || SQLERRM);
+            END;
         END LOOP;
     END LOOP;
 
-    -- Randomly select 3-4 retail customers
-    v_num_retail_customers := TRUNC(DBMS_RANDOM.VALUE(3, 5));
-    
-    -- Process retail customers
-    FOR retail_rec IN (
+    -- Process retail customers (3-4 random customers)
+    FOR retail IN (
         SELECT customer_id 
         FROM customers 
-        WHERE UPPER(last_name) != 'WHOLESALE'
-        ORDER BY DBMS_RANDOM.VALUE()
+        WHERE UPPER(last_name) NOT LIKE '%WHOLESALE%'
+        ORDER BY DBMS_RANDOM.VALUE
+        FETCH FIRST 4 ROWS ONLY
     ) LOOP
-        v_retail_count := v_retail_count + 1;
-        EXIT WHEN v_retail_count > v_num_retail_customers;
-        
-        -- Generate 1-2 orders per retail customer
+        -- Generate 1-2 orders per customer
         FOR i IN 1..TRUNC(DBMS_RANDOM.VALUE(1, 3)) LOOP
-            -- Create order using package procedure
-            order_mgmt_pkg.create_order(
-                p_customer_id => retail_rec.customer_id,
-                p_order_date => p_simulation_date,
-                p_new_order_id => v_order_id
-            );
+            v_items_added := FALSE;
             
-            -- Add 1-3 items per retail order
-            FOR j IN 1..TRUNC(DBMS_RANDOM.VALUE(1, 4)) LOOP
-                -- Try to add an item from available harvest
-                FOR harvest_rec IN (
-                    SELECT 
-                        hc.harvest_id,
-                        hc.available_quantity_kg
-                    FROM harvested_crops hc
-                    WHERE hc.available_quantity_kg > 0
-                    ORDER BY DBMS_RANDOM.VALUE()
-                ) LOOP
-                    -- Calculate random order quantity (0.5-2 kg for retail)
-                    v_order_qty := ROUND(DBMS_RANDOM.VALUE(0.5, 2), 1);
+            -- Create new order using package procedure
+            BEGIN
+                ORDER_MGMT_PKG.CREATE_ORDER(
+                    P_CUSTOMER_ID => retail.customer_id,
+                    P_ORDER_DATE => p_simulation_date,
+                    P_NEW_ORDER_ID => v_order_id
+                );
+                
+                -- Add 1-3 items per order
+                FOR j IN 1..TRUNC(DBMS_RANDOM.VALUE(1, 4)) LOOP
+                    v_successful := FALSE;
                     
-                    IF harvest_rec.available_quantity_kg >= v_order_qty THEN
-                        -- Add order item using package procedure
-                        order_mgmt_pkg.add_order_item(
-                            p_order_id => v_order_id,
-                            p_harvest_id => harvest_rec.harvest_id,
-                            p_quantity_kg => v_order_qty,
-                            p_order_item_id => v_order_item_id
-                        );
-                        EXIT;  -- Move to next item
-                    END IF;
+                    -- Try different harvests until one succeeds or we run out of options
+                    FOR harvest IN (
+                        SELECT h.harvest_id, h.available_quantity_kg
+                        FROM harvested_crops h
+                        WHERE h.available_quantity_kg > 0
+                        AND h.harvest_id NOT IN (
+                            SELECT harvest_id FROM order_items 
+                            WHERE order_id = v_order_id
+                        )
+                        ORDER BY DBMS_RANDOM.VALUE
+                    ) LOOP
+                        EXIT WHEN v_successful;
+                        
+                        BEGIN
+                            -- Generate order quantity (0.5-2kg for retail)
+                            v_order_qty := ROUND(DBMS_RANDOM.VALUE(0.5, LEAST(2, harvest.available_quantity_kg)), 1);
+                            
+                            -- Try to add order item
+                            ORDER_MGMT_PKG.ADD_ORDER_ITEM(
+                                P_ORDER_ID => v_order_id,
+                                P_HARVEST_ID => harvest.harvest_id,
+                                P_QUANTITY_KG => v_order_qty,
+                                P_ORDER_ITEM_ID => v_order_item_id
+                            );
+                            
+                            v_successful := TRUE;
+                            v_items_added := TRUE;
+                        EXCEPTION
+                            WHEN OTHERS THEN
+                                -- Just continue to next harvest
+                                NULL;
+                        END;
+                    END LOOP;
                 END LOOP;
-            END LOOP;
+                
+            EXCEPTION
+                WHEN OTHERS THEN
+                    -- Log error and continue with next order
+                    DBMS_OUTPUT.PUT_LINE('Error processing retail order: ' || SQLERRM);
+            END;
         END LOOP;
     END LOOP;
-    
+
     COMMIT;
+    
+    DBMS_OUTPUT.PUT_LINE('Orders generated for date: ' || TO_CHAR(p_simulation_date, 'DD-MON-YYYY'));
+END generate_daily_orders;
+
+    
+PROCEDURE simulate_week(p_current_date IN DATE DEFAULT SYSDATE) IS
+    v_simulation_date DATE := p_current_date;
+BEGIN
+    -- Simulate 7 days
+    FOR i IN 1..7 LOOP
+        DBMS_OUTPUT.PUT_LINE('=== Simulating ' || TO_CHAR(v_simulation_date, 'DD-MON-YYYY') || ' ===');
+        
+        -- Call to generate sensor readings, progress growth, and generate orders
+        generate_sensor_readings(v_simulation_date);
+        progress_growth_cycles(v_simulation_date);
+        generate_daily_orders(v_simulation_date);
+        
+        -- Advance to next day
+        v_simulation_date := v_simulation_date + 1;
+        
+    END LOOP;
+    
+    COMMIT;  -- Commit after all 7 days of simulation
 EXCEPTION
     WHEN OTHERS THEN
-        ROLLBACK;
-        RAISE_APPLICATION_ERROR(-20004, 'Error generating orders: ' || SQLERRM);
-END generate_daily_orders;
-    
-    
-    
-    
-    PROCEDURE simulate_week(p_current_date IN DATE DEFAULT SYSDATE) IS
-        v_simulation_date DATE := p_current_date;
-    BEGIN
-        -- Simulate 7 days
-        FOR i IN 1..7 LOOP
-            DBMS_OUTPUT.PUT_LINE('=== Simulating ' || TO_CHAR(v_simulation_date, 'DD-MON-YYYY') || ' ===');
-            
-            generate_sensor_readings(v_simulation_date);
-            progress_growth_cycles(v_simulation_date);
-            generate_daily_orders(v_simulation_date);
-            -- Advance to next day
-            v_simulation_date := v_simulation_date + 1;
-            
-        END LOOP;
-        
-        COMMIT;
-    EXCEPTION
-        WHEN OTHERS THEN
-            ROLLBACK;
-            RAISE_APPLICATION_ERROR(-20003, 'Error in simulate_week: ' || SQLERRM);
-    END simulate_week;
+        RAISE_APPLICATION_ERROR(-20003, 'Error in simulate_week: ' || SQLERRM);
+END simulate_week;
+
 END simulation_pkg;
 /
 ---- Run for 4 weeks
